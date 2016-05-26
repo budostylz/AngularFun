81a3c8ef708d752d33f97370a281c9222790f3b1
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-26 19:37:27 -0400

    Add files via upload

---
+++ Basics/Scopes/Scope Functions/README.md
@@ -0,0 +1,15 @@
+﻿##$interval
+
+https://docs.angularjs.org/api/ngMock/service/$interval
+
+##The $interval Service
+
+http://www.w3schools.com/angular/angular_services.asp
+
+##AngularJS ng-model Directive
+
+http://www.w3schools.com/angular/angular_model.asp
+
+##AngularJS ng-click Directive
+
+http://www.w3schools.com/angular/ng_ng-click.asp
---
+++ Basics/Scopes/Scope Functions/app.js
@@ -0,0 +1,25 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+angular.module('myApp').controller('MainController', ['$scope', '$interval', function($scope, $interval){//$scope binds controller with view(index.html)
+    
+    var items = ['bananas', 'apples', 'pears', 'cherries', 'peaches'];
+
+    $scope.itemIndex = null;
+    $scope.currentItem = '';
+
+    $scope.getItem = function () {
+        $scope.currentItem = items[$scope.itemIndex];
+    };
+
+    
+    $interval(function () {
+        $scope.itemIndex = Math.round(Math.random() * (items.length - 1));
+        $scope.getItem();
+
+    }, 2000);
+
+    
+
+
+}]);
