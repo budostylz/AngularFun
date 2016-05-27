b26fa2226a0d17682ba4870687548c81e27df287
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-26 20:18:24 -0400

    Add files via upload

---
+++ Basics/Bracketed Expressions/app.js
@@ -0,0 +1,21 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+angular.module('myApp').controller('MainController', ['$scope', '$interval', function ($scope, $interval) {//$scope binds controller with view(index.html)
+
+    $scope.randomValue = -999; //set property
+
+    $scope.names = ['Wolverine', 'Iron-Man', 'Captain America', 'DeadPool'];
+
+    $scope.qty = 20; $scope.cost = 1.99;
+
+    $scope.pWidth = 100;
+
+    $interval(function () {
+        $scope.randomValue = Math.round(Math.random() * 1000000);
+    }, 2000);
+
+
+
+
+}]);
