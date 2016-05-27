07c943844ef88b12078c76a8d5ef4d4c7fa6f4c7
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-26 20:49:09 -0400

    Add files via upload

---
+++ Basics/Dot Notation/app.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('ParentController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
+
+    $scope.model = {
+        name: 'White Queen'
+    };
+    //$scope.name = "White Queen"; Inheritance Issue
+
+
+}]);
+
+angular.module('myApp').controller('ChildController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
+
+}]);
