275a741c010837c8e065a3a6f4fbbd5f08e55786
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-28 09:04:23 -0400

    Add files via upload

--- Basics/Controllers/Share Data Between Controllers/app.js
+++ Basics/Controllers/Share Data Between Controllers/app.js
@@ -2,24 +2,40 @@
 
 angular.module('myApp', []);
 
+
+/*
+
+First Method Using Nesting Approach
+
 angular.module('myApp').controller('ParentController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
-
     $scope.model = {
-        name: 'White Queen'
+        name: 'Thor'
     };
-    //$scope.name = "White Queen"; Inheritance Issue
+}]);
 
+angular.module('myApp').controller('FirstController', ['$scope', function ($scope) {}]);
+angular.module('myApp').controller('SecondController', ['$scope', function ($scope) {}]);
+
+*/
+
+
+//Service Approach (seem more elegant than nested approach)
+angular.module('myApp').service('SharedService', function () {
+    return { name: 'Hulk' };
+
+});
+
+angular.module('myApp').controller('FirstController', ['$scope', 'SharedService', function ($scope, SharedService) {
+
+    $scope.model = SharedService;
 
 }]);
 
-angular.module('myApp').controller('ChildController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
+angular.module('myApp').controller('SecondController', ['$scope', 'SharedService', function ($scope, SharedService) {
 
-}]);
+    $scope.model = SharedService;
 
-angular.module('myApp').controller('IndependentController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
-    $scope.model = {
-        name: 'Independent Woman'
-    };
+
 }]);
 
 
