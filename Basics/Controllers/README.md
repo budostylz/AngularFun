1f9c442f6c3948d4b88951255cac2ad624ea2b63
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-28 08:21:34 -0400

    Add files via upload

---
+++ Basics/Controllers/README.md
@@ -0,0 +1,7 @@
+﻿##AngularJS References
+
+http://www.w3schools.com/angular/angular_ref_directives.asp
+
+##AngularJS API Docs
+
+https://docs.angularjs.org/api
---
+++ Basics/Controllers/app.js
@@ -0,0 +1,26 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('ParentController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
+
+    $scope.model = {
+        name: 'White Queen'
+    };
+    //$scope.name = "White Queen"; Inheritance Issue
+
+
+}]);
+
+angular.module('myApp').controller('ChildController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
+
+}]);
+
+angular.module('myApp').controller('IndependentController', ['$scope', function ($scope) {//$scope binds controller with view(index.html)
+    $scope.model = {
+        name: 'Independent Woman'
+    };
+}]);
+
+
+
