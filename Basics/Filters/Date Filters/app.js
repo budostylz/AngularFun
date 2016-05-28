bba3cf1a77fdb2f0dc1fa5f89cf70c46861bc09f
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-28 14:25:15 -0400

    Add files via upload

---
+++ Basics/Filters/Date Filters/app.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.now = new Date();
+
+    this.user = {
+        name: 'Johann Schmidt',
+        birthday: 171781200000,
+        lastLogin: 1437705300000
+    };
+
+   
+}]);
