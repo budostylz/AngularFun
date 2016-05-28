94eb3349b39bb57e9fe47e3cbaad9778dc003a4c
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-28 12:22:52 -0400

    Add files via upload

---
+++ Basics/Filters/app.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+
+
+angular.module('myApp').controller('MainController', [function () {
+  
+    this.user = {
+        firstName: 'Stephen',
+        middleName: 'Vincent',
+        lastName: 'Strange',
+        accountType: 'CHECKING',
+        balance: 500000
+    };
+
+}]);
+
