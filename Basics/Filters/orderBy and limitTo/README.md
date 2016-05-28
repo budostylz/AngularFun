33f0261bc843ccd25405ee7322ebe5591ceaefe6
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-28 13:44:29 -0400

    Add files via upload

---
+++ Basics/Filters/orderBy and limitTo/README.md
@@ -0,0 +1,18 @@
+﻿##AngularJS Filters
+
+http://www.w3schools.com/angular/angular_filters.asp
+
+##ngRepeat
+
+https://docs.angularjs.org/api/ng/directive/ngRepeat
+
+##limitTo
+
+https://docs.angularjs.org/api/ng/filter/limitTo
+
+##orderBy
+
+https://docs.angularjs.org/api/ng/filter/orderBy
+
+
+
---
+++ Basics/Filters/orderBy and limitTo/app.js
@@ -0,0 +1,41 @@
+﻿'use strict'
+
+angular.module('myApp', []);
+
+
+angular.module('myApp').controller('MainController', [function () {
+  
+    this.users = [
+        {
+            firstName: 'Tony',
+            lastName: 'Starks',
+            accountType: 'Checking',
+            balance: 1349.2,
+            birthday: 19702905
+        },
+        {
+            firstName: 'Peter',
+            lastName: 'Parker',
+            accountType: 'Checking',
+            balance: 1245.33,
+            birthday: 19640101
+        },
+        {
+            firstName: 'Jean',
+            lastName: 'Gray',
+            accountType: 'Checking',
+            balance: 4788.89,
+            birthday: 19630101
+        },
+        {
+            firstName: 'Felicia',
+            lastName: 'Hardy',
+            accountType: 'Checking',
+            balance: 1349.2,
+            birthday: 19790101
+        }
+
+    ];
+
+   
+}]);
