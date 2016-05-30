fa08c7c9c86c5f7c2c96517a43e874b60f93a956
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-29 22:21:10 -0400

    Add files via upload

---
+++ Basics/Filters/Filter Dropdowns/README.md
@@ -0,0 +1,10 @@
+﻿##ngPattern
+
+https://docs.angularjs.org/api/ng/directive/ngPattern
+
+##ngClick
+
+https://docs.angularjs.org/api/ng/directive/ngClick
+
+
+
---
+++ Basics/Filters/Filter Dropdowns/app.js
@@ -0,0 +1,16 @@
+﻿'use strict'
+angular.module('myApp', []);
+
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.items = [
+        { name: 'Peter Parker', type: 'Avengers' },
+        { name: 'Adam Warlock', type: 'Guardians of the Galaxy' },
+        { name: 'Thunderball', type: 'Illuminati' },
+        { name: 'Grim Reaper', type: 'Lethal Legion' }
+
+    ];
+
+    this.selectedType = ''; //select type
+}]);
