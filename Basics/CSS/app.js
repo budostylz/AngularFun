76cf3e0c7925f723ac866160519ac6d3c3657e58
Shaun Lewis <lewis.shaun1@outlook.com>
2016-05-29 21:48:14 -0400

    Add files via upload

---
+++ Basics/CSS/app.js
@@ -0,0 +1,10 @@
+﻿'use strict'
+angular.module('myApp', []);
+
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.hello = 'Hello, from the Civil War. Yeah, fighting at the airport. You call that a civil war?';
+    this.name = '';
+
+}]);
